bc107ff4c3d026cba02ceb01cccd244f23a64921
Frederic Soc <desarrollador2@wellcopharma.com>
2025-03-10 15:55:46 -0600

    ACreando la coneccion a la base de datos y realizando las primeras consultas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import express from 'express'
-
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Get!!')
 })
 
-app.listen(4000, () => {
-  console.log('Hola')
+app.listen(3000, () => {
+  console.log('Server listening on port http://localhost:3000')
 })
